53ef820959c47332a715870ab611748b057b8beb
kaylie220 <148197573+kaylie220@users.noreply.github.com>
2024-04-19 05:22:39 +0800

    third commit

--- lib/hook.tsx
+++ lib/hook.tsx
@@ -4,14 +4,18 @@ import { useEffect } from "react";
 import { SectionName } from "../lib/types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
-  const { ref, inView } = useInView({ threshold });
+  const { ref, inView } = useInView({
+    threshold,
+  });
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection(sectionName);
     }
-  }, [inView, setActiveSection, timeOfLastClick]);
+  }, [inView, setActiveSection, timeOfLastClick, sectionName]);
 
-  return { ref };
+  return {
+    ref,
+  };
 }
